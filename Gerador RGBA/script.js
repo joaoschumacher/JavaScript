18f8431dc44a94fe45c7800040d50453b85b5548
João Pedro Schumacher <joaopedroschumacher@gmail.com>
2021-07-06 18:49:16 -0300

    update 06-07-2021

--- Gerador RGBA/script.js
+++ Gerador RGBA/script.js
@@ -1,24 +1,22 @@
-let resultado = document.querySelector(".saida");
-let getBody = document.body;
-
 function PegarRGBA() {
+    let resultado = document.getElementById("cores");
     let red = document.getElementById("red").value;
     let green = document.getElementById("green").value;
     let blue = document.getElementById("blue").value;
     let alpha = document.getElementById("alpha").value;
-
-    getBody.style.background=`rgba(${red},${green},${blue},${alpha})`;
-    resultado.innerHTML = `rgba(${red},${green},${blue},${alpha})`;
+    document.body.style.background=`rgba(${red},${green},${blue},${alpha})`;
+    document.getElementById("cores").innerHTML = `rgba(${red},${green},${blue},${alpha})`
 }
 
-function copy(){
-    let element = document.createElement("textarea");
-    element.value = resultado.textContent;
-    document.body.appendChild(element);
-    element.select();
-    document.execCommand("copy")
-    document.body.removeChild(element);
-    alert("Copiado!");
-
+function copiarRGBA() {
+    let cores = document.getElementById("cores");
+    let elemento = document.createElement("textarea");
+    elemento.value = cores.textContent;
+    document.body.appendChild(elemento);
+    elemento.select();
+    document.execCommand(copiarRGBA);
+    document.body.removeChild(elemento);
+    alert("copiado!");
 }
+
 
